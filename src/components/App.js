7091c665bef467555ffd87290eb4adb078b2bb6a
osha7 <ogroetz@gmail.com>
2020-09-18 19:02:12 -0600

    start of routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import '../App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './home/home'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Home /> 
+      </BrowserRouter>
     </div>
   );
 }
