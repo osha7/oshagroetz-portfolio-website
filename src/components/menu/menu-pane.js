c1db8902189c7ac06d8d72d617945e1937556bc7
osha7 <ogroetz@gmail.com>
2020-09-18 21:41:24 -0600

    change made from NavLink to Link in menupane

--- src/components/menu/menu-pane.js
+++ src/components/menu/menu-pane.js
@@ -1,42 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 
-// function MenuPane() {
-//   return (
-//     <div className="menu-pane">
-       
-                
-//                     <a href="###">Home</a>
-//                     <span id="DC-info">
-//                         <p>...is where I want to be, this must be the place</p>
-//                     </span>
-//                 </li>
-//                 <li><a href="###">Portfolio</a></li>
-//                 <li><a href="###">About</a></li>
-//                 <li><a href="###">Resume</a></li>
-//                 <li><a href="###">Contact</a></li>
-//             </ul>
-//         </nav>
-//   </div>
-//   );
-// }
-
-// export default MenuPane;
-
-// ------------
 const MenuPane = () => {
     return (
         <div className="menu-pane">
             <nav className="nav">
                 <ul className="menu-links">
-                    <li><NavLink to="/" exact >Home</NavLink>
+                    <li><Link to="/" exact >Home</Link>
                     <span id="DC-info">
                         <p>...is where I want to be, this must be the place</p>
                     </span></li>
-                    <li><NavLink to="/portfolio" exact >Portfolio</NavLink></li>
-                    <li><NavLink to="/about" exact >About</NavLink></li>
-                    <li><NavLink to="/resume" exact >Resume</NavLink></li>
-                    <li><NavLink to="/contact" exact >Contact</NavLink></li>
+                    <li><Link to="/portfolio" exact >Portfolio</Link></li>
+                    <li><Link to="/about" exact >About</Link></li>
+                    <li><Link to="/resume" exact >Resume</Link></li>
+                    <li><Link to="/contact" exact >Contact</Link></li>
+                    {/* NavLink would be used instead if using 'active styling' */}
                 </ul>
             </nav>
         </div>
