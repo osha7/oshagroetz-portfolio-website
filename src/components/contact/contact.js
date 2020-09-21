89d9935fe9e0f0a90618dab033df6e1ac267bc2e
osha7 <ogroetz@gmail.com>
2020-09-21 14:58:24 -0600

    contact page work

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,6 +4,12 @@ export default function Contact() {
     return (
         <div className="contact-page">
             <h1>...contact &nbsp;</h1>
+            <br /><br />
+            <a href="https://www.linkedin.com/in/jennifer-osha-groetz-0318b16b/" target="_blank" title="LinkedIn" ><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp;
+            <a href="https://github.com/osha7" target="_blank" title="Github" ><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp;
+            {/* <a href="" target="_blank" ><i class="fa fa-envelope fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp; */}
+            <a href="https://medium.com/@ogroetz" target="_blank" title="Medium Blog" ><i class="fa fa-medium fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp;
+            
         </div>
     )
 }
