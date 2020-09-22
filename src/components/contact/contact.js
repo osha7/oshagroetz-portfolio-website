f2ba993199b7c5491b0bef8570333763baf6901d
osha7 <ogroetz@gmail.com>
2020-09-21 18:28:04 -0600

    contact page finished

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -4,12 +4,18 @@ export default function Contact() {
     return (
         <div className="contact-page">
             <h1>...contact &nbsp;</h1>
-            <br /><br />
-            <a href="https://www.linkedin.com/in/jennifer-osha-groetz-0318b16b/" target="_blank" title="LinkedIn" ><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp;
-            <a href="https://github.com/osha7" target="_blank" title="Github" ><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp;
-            {/* <a href="" target="_blank" ><i class="fa fa-envelope fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp; */}
-            <a href="https://medium.com/@ogroetz" target="_blank" title="Medium Blog" ><i class="fa fa-medium fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp;
-            
+            <div className="links">
+                <br /><br />
+                <a href="https://www.linkedin.com/in/jennifer-osha-groetz-0318b16b/" target="_blank" title="LinkedIn" ><i class="fa fa-linkedin-square fa-3x" aria-hidden="true"></i></a>&nbsp;&nbsp;
+                <a href="https://github.com/osha7" target="_blank" title="Github" ><i class="fa fa-github-square fa-3x" aria-hidden="true"></i></a>&nbsp;&nbsp;
+                {/* <a href="" target="_blank" ><i class="fa fa-envelope fa-3x" aria-hidden="true"></i></a><br /><br />&nbsp;&nbsp; */}
+                <a href="https://medium.com/@ogroetz" target="_blank" title="Medium Blog" ><i class="fa fa-medium fa-3x" aria-hidden="true"></i></a>&nbsp;&nbsp;
+            </div>
+            <div className="contact-info">
+                <p><strong>Osha Groetz</strong><br />
+                ogroetz@gmail.com<br />
+                917-477-9485</p>
+            </div>
         </div>
     )
 }
--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -3,6 +3,7 @@ import React from "react";
 const Resume = () =>{
   return(
       <div className="resume">
+        
         <div id="elfsight" className="elfsight-app-f28c992d-2739-4f57-847a-0bb0bf620106"></div>
       </div>
   )
