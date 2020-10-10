889d4886dd99d028efb19e55a6fad0924dd0a80a
osha7 <ogroetz@gmail.com>
2020-10-10 11:46:10 -0600

    update to about me

--- src/components/about/me.js
+++ src/components/about/me.js
@@ -9,7 +9,7 @@ export default function About() {
                     {/* <h2>Pro-Skills:</h2> */}
                     {/* <h2>Plugged-In:</h2> */}
                     Proactive individual with strong skills in project management, identifying and troubleshooting critical issues and team collaboration. 
-                    Possessing an acute eye for graphic design & marketing with a passion for innovative tech. Experienced in React.JS and Ruby on Rails based programming with a background in business development and start-up growth.
+                    Possessing an acute eye for design & marketing with a passion for innovative tech. Experienced in React.JS and Ruby on Rails based programming with a background in business development and start-up growth.
                     <br/><br/>With a background in fashion and restaurants, at the start of 2020 I realized it was time to make a change.
                     Thanks to Flatiron Schools unbelievable Software Engineering program, and having graduated in September 2020, I'm now starting a new life in coding.
                     <br/><br/>
