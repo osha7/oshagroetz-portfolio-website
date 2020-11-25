910ec0204611c2e7cbd743f2be43922b8f024684
osha7 <ogroetz@gmail.com>
2020-11-25 10:57:47 -0700

    revamp of home / navbar / blog pages

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import HomeBody from './home-body'
+import Shapes from './shapes'
 
 function Home() {
   return (
     <div className="menu-container">
         <HomeBody />
+        <Shapes />
     </div>
   );
 }
---
+++ src/components/home/shapes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../css/home.css';
+
+function Shapes() {
+  return (
+    <>
+        <div className="block glow">
+          <div className="empty"></div>
+        </div>
+    </>
+  );
+}
+
+export default Shapes;
